e4a12982423a4ca97e17a231439989a95b1f6fc4
zeldace <yanlanglois33830@gmail.com>
2022-03-28 11:17:24 +0200

    class offer

---
+++ test_MVC/models/Offer.php
@@ -0,0 +1,21 @@
+<?php
+class Offer
+{
+    private $IdOffer;
+    private $Duration;
+    private $Compensation;
+    private $DateOffer;
+    private $NumberPlace;
+    private $OfferName;
+    private $StreetName;
+    private $StreetNumber;
+    private $City;
+    private $PostalCode;
+    private $Promotion;
+    private $Subject;
+    private $Name;
+    private $InternNumber;
+    private $Sector;
+    private $PilotTrust;
+
+}
