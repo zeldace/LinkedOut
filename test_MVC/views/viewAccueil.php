9a068bade4b42dcafbb6c8bfbe0a05a7d960c718
vincent verlaan <vincentverlaan33@outlook.fr>
2022-03-28 16:22:14 +0200

    indexmodifs

--- test_MVC/views/viewAccueil.php
+++ test_MVC/views/viewAccueil.php
@@ -20,10 +20,6 @@ session_unset();
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <meta name="description" content="This website is used to manage the different users of LinkedOut. This is accessible only by the admin">
   <meta name="theme-color" content="#317EFB"/>
-  <link rel="icon" type="image" href="favicon.png">
-  <link rel="manifest" href="manifest.json">
-  <link rel="serviceWorker" href="ServiceWorker.js">
-  <link rel="apple-touch-icon" href="https://cdn.glitch.com/49d34dc6-8fbd-46bb-8221-b99ffd36f1af%2Ftouchicon-180.png?v=1566411949736%22%3E">
   <style>
     .bd-placeholder-img {
       font-size: 1.125rem;
